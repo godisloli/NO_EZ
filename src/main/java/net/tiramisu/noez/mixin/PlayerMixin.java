3c41b155f6e592b0ecf71c3b6f10d7e24838df2c
ggtmb <nhattung2801@gmail.com>
2024-12-01 17:19:37 +0700

    from Tiramisu second pc

--- src/main/java/net/tiramisu/noez/mixin/PlayerMixin.java
+++ src/main/java/net/tiramisu/noez/mixin/PlayerMixin.java
@@ -1,10 +1,20 @@
 package net.tiramisu.noez.mixin;
 
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SwordItem;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Player.class)
-public abstract class PlayerMixin {
-
+@Mixin(value = SwordItem.class, remap = false)
+public class PlayerMixin{
+    @Inject(method = "canPerformAction", at = @At("HEAD"), cancellable = true)
+    private void noSweepingAttack(ItemStack itemStack, ToolAction toolAction, CallbackInfoReturnable<Boolean> cir){
+        if (toolAction == ToolActions.SWORD_SWEEP){
+            cir.setReturnValue(false);
+        }
+    }
 }
-
