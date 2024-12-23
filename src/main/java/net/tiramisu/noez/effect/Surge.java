f86b289967ce8cf9fafdb3798ba605e038345456
BasementLoli <nhattung2801@gmail.com>
2024-12-23 19:50:39 +0700

    from BasementLoli

--- src/main/java/net/tiramisu/noez/effect/Surge.java
+++ src/main/java/net/tiramisu/noez/effect/Surge.java
@@ -5,9 +5,12 @@ import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraft.world.entity.projectile.ThrownPotion;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber
 public class Surge extends MobEffect {
     private static final float DAMAGE_PER_LEVEL = 0.25f;
 
@@ -16,15 +19,22 @@ public class Surge extends MobEffect {
     }
 
     @SubscribeEvent
-    public void mamgicSurge(LivingAttackEvent event) {
-        Entity entity = event.getSource().getEntity();
-        if (entity instanceof LivingEntity attacker) {
-            if (attacker.hasEffect(this)) {
-                if (isMagicDamage(event.getSource())) {
-                    int amplifier = attacker.getEffect(this).getAmplifier() + 1;
+    public void mamgicSurge(LivingHurtEvent event) {
+        DamageSource source = event.getSource();
+        System.out.println("Damage Source: " + source.getMsgId());
+        Entity attackerEntity = source.getEntity();
+        Entity directEntity = source.getDirectEntity();
+        if (isMagicDamage(source)) {
+            if (attackerEntity instanceof LivingEntity attacker && attacker.hasEffect(this)) {
+                int amplifier = attacker.getEffect(this).getAmplifier() + 1;
+                float boostedDamage = event.getAmount() * (1 + (DAMAGE_PER_LEVEL * amplifier));
+                event.setAmount(boostedDamage);
+            }
+            if (directEntity instanceof ThrownPotion potion && potion.getOwner() instanceof LivingEntity thrower) {
+                if (thrower.hasEffect(this)) {
+                    int amplifier = thrower.getEffect(this).getAmplifier() + 1;
                     float boostedDamage = event.getAmount() * (1 + (DAMAGE_PER_LEVEL * amplifier));
-                    event.setCanceled(true);
-                    event.getEntity().hurt(attacker.damageSources().magic(), boostedDamage);
+                    event.setAmount(boostedDamage);
                 }
             }
         }
