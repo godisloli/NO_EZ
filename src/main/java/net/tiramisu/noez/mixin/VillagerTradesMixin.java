5f4217af1d4b7268814809e5133b80bc04742813
BasementLoli <nhattung2801@gmail.com>
2025-01-22 15:10:23 +0700

    from BasementLoli

--- src/main/java/net/tiramisu/noez/mixin/VillagerTradesMixin.java
+++ src/main/java/net/tiramisu/noez/mixin/VillagerTradesMixin.java
@@ -2,15 +2,17 @@ package net.tiramisu.noez.mixin;
 
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.entity.npc.VillagerTrades.ItemListing;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(targets = "net.minecraft.world.entity.npc.VillagerTrades$EnchantedItemForEmeralds")
-public class VillagerTradeMixin {
-    @Inject(method = "getOffer", at = @At("HEAD"), cancellable = true)
-    private void removeEnchantedTrades(CallbackInfoReturnable<MerchantOffer> cir) {
+@Mixin(targets = "net.minecraft.world.entity.npc.VillagerTrades$EmeraldForItems")
+public abstract class VillagerTradesMixin implements ItemListing {
+
+    @Inject(method = "getOffer", at = @At("RETURN"), cancellable = true)
+    private void removeEnchantedItemTrades(CallbackInfoReturnable<MerchantOffer> cir) {
         MerchantOffer offer = cir.getReturnValue();
         if (offer != null && !EnchantmentHelper.getEnchantments(offer.getResult()).isEmpty()) {
             cir.setReturnValue(null);
