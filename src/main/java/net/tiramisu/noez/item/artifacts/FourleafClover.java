d28cc508d17ce59ba574246b21a880a2ff16da13
BasementLoli <nhattung2801@gmail.com>
2025-04-29 22:44:50 +0700

    from BasementLoli

---
+++ src/main/java/net/tiramisu/noez/item/artifacts/FourleafClover.java
@@ -0,0 +1,12 @@
+package net.tiramisu.noez.item.artifacts;
+
+import net.minecraft.world.item.Item;
+import net.tiramisu.noez.item.NoezArtifacts;
+
+public class FourleafClover extends Item implements NoezArtifacts {
+    public FourleafClover(Properties pProperties) {
+        super(pProperties);
+    }
+
+
+}
