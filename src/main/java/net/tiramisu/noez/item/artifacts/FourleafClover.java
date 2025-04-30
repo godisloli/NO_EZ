abfb619ab3e4b4caaec76bd3b392cb8a32f68395
BasementLoli <nhattung2801@gmail.com>
2025-04-30 17:49:44 +0700

    from BasementLoli

--- src/main/java/net/tiramisu/noez/item/artifacts/FourleafClover.java
+++ src/main/java/net/tiramisu/noez/item/artifacts/FourleafClover.java
@@ -1,6 +1,11 @@
 package net.tiramisu.noez.item.artifacts;
 
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.tiramisu.noez.item.NoezArtifacts;
 
 public class FourleafClover extends Item implements NoezArtifacts {
@@ -8,5 +13,8 @@ public class FourleafClover extends Item implements NoezArtifacts {
         super(pProperties);
     }
 
-
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        return super.use(pLevel, pPlayer, pUsedHand);
+    }
 }
