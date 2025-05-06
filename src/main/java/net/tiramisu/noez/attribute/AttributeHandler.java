36d9de1e90f5d4a068836d7f2767e73d18b6ed7d
loliisgod <nhattung2801>
2025-05-06 10:21:55 +0700

    from Tiramisu

--- src/main/java/net/tiramisu/noez/attribute/AttributeHandler.java
+++ src/main/java/net/tiramisu/noez/attribute/AttributeHandler.java
@@ -19,7 +19,6 @@ import net.minecraftforge.event.entity.player.TradeWithVillagerEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.tiramisu.noez.item.NoezItems;
 import net.tiramisu.noez.util.NoezTags;
 
 import java.util.List;
